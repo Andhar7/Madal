0b5b753903e3749a356cb0583070063032e48eba
Andrej Kling <boxingprofi@web.de>
2021-01-27 15:51:47 +0100

    Initial Commit

---
+++ Madal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Madal
+//
+//  Created by Andrej Kling on 27.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Madal/MadalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MadalApp.swift
+//  Madal
+//
+//  Created by Andrej Kling on 27.01.21.
+//
+
+import SwiftUI
+
+@main
+struct MadalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
